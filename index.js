6f97bdbd03d04400af0d2007067aac9ab2109254
Muhammad Althaf Rahman <muhammad.rahman@niveussolutions.com>
2023-07-26 16:16:09 +0530

    Add function to create file using fs module
    add sample file, which is created using fs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const promise = new Promise((res, reject) => {
     res('message');
 });
@@ -5,3 +6,15 @@ const promise = new Promise((res, reject) => {
 promise.then(data => {
     console.log(data)
 })
+
+const makeFile = async(fileName, content) => {
+    try {
+        await fs.writeFile(fileName, content, (err) => {
+            if(err) throw err;
+        });
+    } catch (err) {
+        console.log('Failed with error', err);
+    }
+}
+
+makeFile('node-lms-assignment.txt', 'hello from office');
